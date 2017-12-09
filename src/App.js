ada6c2643094a8f4cc31d0c7c93863a30bd18060
Pablo <pablosjv@hotmail.com>
2017-12-09 10:38:41 +0100

    React Bootstrap included

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Grid, Row } from 'react-bootstrap';
 
 const data = [
   {
@@ -63,9 +64,15 @@ class App extends Component {
 
     return (
       <div className="App">
-        <Search onChange={this.searchValue} value={searchTerm}>
-            Search here
-        </Search>
+        <Grid>
+          <Row>
+            <div className="jumbotron">
+              <Search onChange={this.searchValue} value={searchTerm}>
+                Search here
+              </Search>
+            </div>
+          </Row>
+        </Grid>
 
         <Table
           data={data}
@@ -77,12 +84,45 @@ class App extends Component {
   }
 }
 
+// STATELESS COMPONENTS AS ANONYMOUS FUNCTIONS
+// const Search = ({ onChange, value, children }) => {
+//   <form>
+//     {children}
+//     <input type="text" onChange={onChange} value={value} />
+//   </form>
+// };
+// const Table = ({ data, searchTerm, removeItem }) => {
+//   <div>
+//     {data.filter(isSearched(searchTerm)).map(item => (
+//       <div key={item.objectID}>
+//         <h1>
+//           {' '}
+//           <a href={item.url}>{item.title}</a> by {item.author}
+//         </h1>
+//         <h4>
+//           {item.num_comments} Comments | {item.points} points
+//         </h4>
+//         <Button type="button" onClick={() => removeItem(item.objectID)}>
+//           Remove
+//         </Button>
+//       </div>
+//     ))}
+//   </div>
+// };
+
+const Button = ({ type, onClick, children }) => (
+  <button type={type} onClick={onClick}>
+    {children}
+  </button>
+);
+
+// CLASS VERSION OF THE COMPONETS
 class Search extends Component {
   // constructor() {}
   render() {
     return (
       <form>
-          {this.props.children}
+        {this.props.children}
         <input
           type="text"
           onChange={this.props.onChange}
@@ -92,10 +132,9 @@ class Search extends Component {
     );
   }
 }
-
 class Table extends Component {
   render() {
-    const { data, searchTerm, removeItem } = this.props;
+    const {data, searchTerm, removeItem} = this.props;
     return (
       <div>
         {data.filter(isSearched(searchTerm)).map(item => (
@@ -107,14 +146,25 @@ class Table extends Component {
             <h4>
               {item.num_comments} Comments | {item.points} points
             </h4>
-            <button type="button" onClick={() => removeItem(item.objectID)}>
-              Remove item
-            </button>
+            <Button type="button" onClick={() => removeItem(item.objectID)}>
+              Remove
+            </Button>
           </div>
         ))}
       </div>
     );
   }
 }
+// class Button extends Component {
+//   render() {
+//     const { type, onClick, children } = this.props;
+//     return (
+//       <button type={type} onClick={onClick}>
+//         {children}
+//       </button>
+//     );
+//   }
+// }
+
 
 export default App;
