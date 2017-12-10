4b3f9901f4d242e4b85d46596202fd627760baa0
Pablo <pablosjv@hotmail.com>
2017-12-10 20:15:42 +0100

    Added navigation with react-router-dom

--- src/index.js
+++ src/index.js
@@ -2,7 +2,33 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  NavLink
+} from 'react-router-dom';
+import {Navbar, Nav, NavItem} from 'react-bootstrap';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Root = () => (
+  <Router>
+    <div>
+      <Navbar>
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+      </Navbar>
+      <Route exact to="/" component={App} />
+      <Route exact to="/about" component={About} />
+    </div>
+  </Router>
+);
+
+const About = () => (
+    <div>
+        <h1>This is about page</h1>
+    </div>
+)
+
+ReactDOM.render(<Root />, document.getElementById('root'));
 registerServiceWorker();
